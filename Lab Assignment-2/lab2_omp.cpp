8c806602b1bb3a102d176e342c56de76a1facf3e
Pradyumna Meena <pradyumnameena@gmail.com>
2019-03-17 13:34:43 +0530

    PCA algorithm sequential code almost complete

--- Lab Assignment-2/lab2_omp.cpp
+++ Lab Assignment-2/lab2_omp.cpp
@@ -4,10 +4,6 @@
 #include "lab2_omp.h"
 using namespace std;
 
-float* matrix_mult(int m1,int n1,int m2,int n2,float* mat1,float* mat2);
-
-float* matrix_transpose(int m,int n,float* mat);
-
 float* matrix_mult(int m1,int n1,int m2,int n2,float* mat1,float* mat2){
 	float* res_matrix = (float*)malloc(sizeof(float)*m1*n2);
 	for(int i = 0;i<m1*n2;i++){
@@ -39,18 +35,59 @@ void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 	float* D_transpose = matrix_transpose(M,N,D);
 	float* matrix_prod = matrix_mult(M,N,N,M,D,D_transpose);
 	free(D_transpose);
-	
-	// computing the eigen values and sorting them in descending order
-		while(true){
-			for(i = 0;i<M;i++){
-				for(j = 0;j<M;j++){
-
-				}
-			}
-		}
 }
 
 void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** D_HAT, int *K)
 {
+    // sigma has square root of eigen values hence all will be positive
+    int i = 0;
+    int j = 0;
+    int idx = 0;
+    int index = 0;
+    float sum = 0;
+    float temp = 0.0;
+    float* reduced_variance = (float*)malloc(sizeof(float)*N);
+    int* eigen_value_sorted_index = (int*)malloc(sizeof(int)*N);
+
+    for(idx = 0;idx<N;idx++){
+    	sum+=(*(SIGMA+idx));
+    }
     
+    for(idx = 0;idx<N;idx++){
+    	*(reduced_variance+idx) = (*(SIGMA+idx))/sum;
+    	*(eigen_value_sorted_index+i) = idx;
+    }
+
+    for(i = 0;i<N-1;i++){
+    	for(j = i+1;j<N;j++){
+    		if((*(reduced_variance+j)) > (*(reduced_variance+i))){
+    			temp = *(reduced_variance+j);
+    			*(reduced_variance+j) = *(reduced_variance+i);
+    			*(reduced_variance+i) = temp;
+    			*(eigen_value_sorted_index+i) = j;
+    			*(eigen_value_sorted_index+j) = i;
+    		}
+    	}
+    }
+    // eigen_value_sorted_index stores ki is index par kaunse index ka eigenvector ki cooresponding value hai
+
+    idx = 0;
+    float summed_variance = 0;
+    float retention_reqd = retention*0.01;
+    for(idx = 0;idx<N;idx++){
+    	summed_variance+=(*(reduced_variance+idx));
+    	if(summed_variance>=retention_reqd){
+    		*K = idx+1;
+    		break;
+    	}
+    }
+
+    // idx+1 is the number of features to be used
+    int col = 0;
+    int to_be_used = 0;
+    float* reduced_eigen_vector_matrix = (float*)malloc(sizeof(float)*(idx+1)*M);
+
+    // write function to retrieve the corresponding eigen vectors
+
+    *(D_HAT) = matrix_mult(M,N,M,1+idx,D,reduced_eigen_vector_matrix);
 }
