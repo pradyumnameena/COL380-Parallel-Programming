a91b0865cefe78a42921bd2da5f1fb4f67305635
Pradyumna Meena <pradyumnameena@gmail.com>
2019-03-19 12:23:02 +0530

    sequential code complete and debugged and tested. Parallelization left

--- Lab Assignment-2/lab2_omp.cpp
+++ Lab Assignment-2/lab2_omp.cpp
@@ -32,6 +32,10 @@ void matrix_transpose(int m,int n,float* mat,float* res_matrix){
 	return;
 }
 
+void matrix_mult_with_transpose(int m1,int n1,int m2,int n2,float* mat1,float* mat2,float* result_matrix){
+
+}
+
 void copy_matrix_values(float* mat1,float* mat2,int num_vals){
 	#pragma omp parallel for
 	{
@@ -56,7 +60,7 @@ void identity_matrix(float* mat,int M){
 		#pragma omp for
 		{
 			for(i = 0;i<M;i++){
-				*(mat+i*M+i) = 1;
+				*(mat+i*M+i) = 1.0;
 			}
 		}
 	}
@@ -163,113 +167,198 @@ void householders(int m,float* matrix,float* Q,float* R){
 	return;
 }
 
-void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
-{	
-	float* D_transpose = (float*)malloc(sizeof(float)*N*M);
-	float* matrix_prod = (float*)malloc(sizeof(float)*M*M);
-	matrix_transpose(M,N,D,D_transpose);
-	matrix_mult(M,N,N,M,D,D_transpose,matrix_prod);
-	free(D_transpose);
-	
-	float* QR_decomposition_Q = (float*)malloc(sizeof(float)*M*M);
-	float* QR_decomposition_R = (float*)malloc(sizeof(float)*M*M);
-	identity_matrix(QR_decomposition_Q,M);
-	identity_matrix(QR_decomposition_R,M);
-	float* D_original = (float*)malloc(sizeof(float)*M*M);
-	float* E_original = (float*)malloc(sizeof(float)*M*M);
-	float* E_new = (float*)malloc(sizeof(float)*M*M);
-	
-	int row = 0;
-	int col = 0;
-	int num_iters = 0;
-	bool condition = true;
-	int iterations_limit = 1;
-	
-	copy_matrix_values(matrix_prod,D_original,M*M);
-	identity_matrix(E_original,M);
-	identity_matrix(E_new,M);
+float compute_error(float* mat1,float* mat2,int num){
+	float rv = 0;
+	int idx = 0;
+	for(idx = 0;idx<num;idx++){
+		*(mat2+idx) -= (*(mat1+idx));
+		*(mat2+idx) *= (*(mat2+idx));
+		rv += (*(mat2+idx));
+	}
+	return sqrt(rv);
+}
 
-	while(condition){
-		householders(M,D_original,QR_decomposition_Q,QR_decomposition_R);
-		matrix_mult(M,M,M,M,QR_decomposition_R,QR_decomposition_Q,D_original);
-		matrix_mult(M,M,M,M,E_original,QR_decomposition_Q,E_new);
-		copy_matrix_values(E_new,E_original,M*M);
-		condition = (num_iters<iterations_limit-1);
-		num_iters++;
+void get_columns(float* mat,float* mat2,int length,int num_cols){
+	int i = 0;
+	int j = 0;
+
+	for(i = 0;i<length;i++){
+		for(j = 0;j<num_cols;j++){
+			*(mat2+length*i+j) = *(mat+i*length+j);
+		}
 	}
 	return;
 }
 
-void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** D_HAT, int *K)
+void matrix_print(int rows,int cols,float* pointer){
+	int i = 0;
+	int j = 0;
+	for(i = 0;i<rows;i++){
+		for(j = 0;j<cols;j++){
+			printf("%f, ",*(pointer+i*cols+j));
+		}
+		printf("\n");
+	}
+}
+
+void SVD(int M, int N, float* D, float** U, float** SIGMA, float** V_T)
 {	
-	printf("PCA AREA\n");
-    // sigma has square root of eigen values hence all will be positive
-    int i = 0;
-    int j = 0;
-    int idx = 0;
-    int index = 0;
-    float sum = 0;
-    float temp = 0.0;
-    float* reduced_variance = (float*)malloc(sizeof(float)*N);
+	float* D_transpose = (float*)malloc(sizeof(float)*N*M);
+	float* matrix_prod = (float*)malloc(sizeof(float)*N*N);
+	matrix_transpose(M,N,D,D_transpose);
+	matrix_mult(N,M,M,N,D_transpose,D,matrix_prod);
+
+	float* QR_decomposition_Q = (float*)malloc(sizeof(float)*N*N);
+	float* QR_decomposition_R = (float*)malloc(sizeof(float)*N*N);
+	float* D_original = (float*)malloc(sizeof(float)*N*N);
+	float* D_old = (float*)malloc(sizeof(float)*N*N);
+	float* E_original = (float*)malloc(sizeof(float)*N*N);
+	float* E_new = (float*)malloc(sizeof(float)*N*N);
+
+	int num_iters = 0;
+	float epsilon = 0.001;
+	float error = 10;
+	int max_iterations = 30000;
+
+	copy_matrix_values(matrix_prod,D_original,N*N);
+	free(matrix_prod);
+	identity_matrix(E_original,N);
+	identity_matrix(E_new,N);
+	
+	while(error>epsilon && num_iters<max_iterations){
+		identity_matrix(QR_decomposition_Q,N);
+		identity_matrix(QR_decomposition_R,N);
+		householders(N,D_original,QR_decomposition_Q,QR_decomposition_R);
+		copy_matrix_values(D_original,D_old,N*N);
+		matrix_mult(N,N,N,N,QR_decomposition_R,QR_decomposition_Q,D_original);
+		matrix_mult(N,N,N,N,E_original,QR_decomposition_Q,E_new);
+		copy_matrix_values(E_new,E_original,N*N);
+		error = compute_error(D_original,D_old,N*N);
+		num_iters++;
+	}
+	printf("Number of iterations = %d\n",num_iters);
+	printf("Error = %f\n",error);
+
+	printf("Eigen vector matrix\n");
+	matrix_print(N,N,E_new);
+	printf("Eigen values matrix\n");
+	matrix_print(N,N,D_original);
+	printf("*************\n");
+
+	free(QR_decomposition_Q);
+	free(QR_decomposition_R);
+	free(E_original);
+	free(D_old);
+	
+	int idx = 0;
+	float* eigen_values = (float*)malloc(sizeof(float)*N);
     int* eigen_value_sorted_index = (int*)malloc(sizeof(int)*N);
-
-    // sum of eigen values
-    for(idx = 0;idx<N;idx++){
-    	sum+=(*(SIGMA+idx));
-    }
     
-    // reduction variances
-    #pragma omp parallel for
-    {
-    	for(idx = 0;idx<N;idx++){
-	    	*(reduced_variance+idx) = (*(SIGMA+idx))/sum;
-	    	*(eigen_value_sorted_index+idx) = idx;
-	    }
-    }
+	for(idx = 0;idx<N;idx++){
+		*(D_original+idx*N+idx) = sqrt(abs((*(D_original+idx*N+idx))));
+		*(eigen_values+idx) = *(D_original+idx*N+idx);
+		*(eigen_value_sorted_index+idx) = idx;
+	}
 
-    // sorting the eigen values while keeping track of the corresponding column
+	int i = 0;
+	int j = 0;
+    float temp = 0;
     for(i = 0;i<N-1;i++){
     	for(j = i+1;j<N;j++){
-    		if((*(reduced_variance+j)) > (*(reduced_variance+i))){
-    			temp = *(reduced_variance+j);
-    			*(reduced_variance+j) = *(reduced_variance+i);
-    			*(reduced_variance+i) = temp;
+    		if((*(eigen_values+j)) > (*(eigen_values+i))){
+    			temp = *(eigen_values+j);
+    			*(eigen_values+j) = *(eigen_values+i);
+    			*(eigen_values+i) = temp;
     			*(eigen_value_sorted_index+i) = j;
     			*(eigen_value_sorted_index+j) = i;
     		}
     	}
     }
-
-    // eigen_value_sorted_index stores ki is index par kaunse index ka eigenvector ki cooresponding value hai
+    
     idx = 0;
-    float summed_variance = 0;
-    float retention_reqd = retention*0.01;
+    int diag_idx = 0;
+    float* eigen_values_pointer = *(SIGMA);
+    float* Sigma_inverse = (float*)malloc(sizeof(float)*M*N);
+
     for(idx = 0;idx<N;idx++){
-    	summed_variance+=(*(reduced_variance+idx));
-    	if(summed_variance>=retention_reqd){
-    		*K = idx+1;
+		*(eigen_values_pointer+idx) = *(eigen_values+idx);
+	}
+
+	free(eigen_values);
+
+    idx = 0;
+	for(idx = 0;idx<N*M;idx++){
+		*(Sigma_inverse+idx) = 0;
+	}
+	
+	for(diag_idx = 0;diag_idx<N;diag_idx++){
+		*(Sigma_inverse+diag_idx*N+diag_idx) = 1/(*(eigen_values_pointer+diag_idx));
+	}
+	
+	idx = 0;
+    int row = 0;
+    int index = 0;
+    float* eigen_vector_pointer = *(U);
+    for(idx = 0;idx<N;idx++){
+    	index = *(eigen_value_sorted_index+idx);
+    	for(row = 0;row<N;row++){
+    		*(eigen_vector_pointer+row*N+idx) = *(E_new+row*N+index);
+    	}
+    }
+
+	free(eigen_value_sorted_index);
+	float* U_transpose = (float*)malloc(sizeof(float)*N*N);
+    matrix_transpose(N,N,eigen_vector_pointer,U_transpose);
+    matrix_mult(M,N,N,N,Sigma_inverse,U_transpose,Sigma_inverse);
+    free(U_transpose);
+    matrix_mult(M,N,N,M,Sigma_inverse,D_transpose,*(V_T));
+    free(D_transpose);
+    free(Sigma_inverse);
+	return;
+}
+
+void PCA(int retention, int M, int N, float* D, float* U, float* SIGMA, float** D_HAT, int *K)
+{	
+	int idx = 0;
+    float sum = 0;
+    float retention_reqd = retention*0.01;
+    float* reduced_variance = (float*)malloc(sizeof(float)*N);
+
+    // sum of eigen values
+    for(idx = 0;idx<N;idx++){
+    	sum+=(*(SIGMA+idx));
+    }
+
+    // reduction variances
+    #pragma omp parallel for
+    {
+    	for(idx = 0;idx<N;idx++){
+	    	*(reduced_variance+idx) = (*(SIGMA+idx))/sum;
+	    }
+    }
+
+    sum = 0.0;
+    for(idx = 0;idx<N;idx++){
+    	sum+=(*(reduced_variance+idx));
+    	if(sum>=retention_reqd){
     		break;
     	}
     }
-    
-    // trivial/corner case
+
     if(retention==100){
     	idx = N-1;
     }
+    *K = 1+idx;
 
-    // idx+1 is the number of features to be used
-    int col = 0;
-    int to_be_used = 0;
-    float* reduced_eigen_vector_matrix = (float*)malloc(sizeof(float)*(idx+1)*M);
-   
-    // function to retrieve the corresponding eigen vectors
-    for(index = 0;index<=idx;index++){
-    	to_be_used = *(eigen_value_sorted_index+index);
-    	for(col=0;col<M;col++){
-    		*(reduced_eigen_vector_matrix+col*(1+idx)+index) = *(U+col*N+to_be_used);
-    	}
-    }
- 	printf("End\n");
-    matrix_mult(M,N,M,1+idx,D,reduced_eigen_vector_matrix,*(D_HAT));
-    printf("AEnd\n");
+    free(reduced_variance);
+    float* W = (float*)malloc(sizeof(float)*(1+idx)*N);
+    get_columns(U,W,N,1+idx);
+    float* D_HAT_pointer = (float*)malloc(sizeof(float)*(1+idx)*M);
+    *D_HAT = D_HAT_pointer;
+    matrix_mult(M,N,N,1+idx,D,W,D_HAT_pointer);
+    free(W);
+    return;
 }
+
+
+
