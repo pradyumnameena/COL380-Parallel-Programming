34dff476586aae3cec8e7b68152f79e24568402f
Pradyumna Meena <pradyumnameena@gmail.com>
2020-02-21 23:23:59 +0530

    removed extra files and updated compile.sh

---
+++ A1/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$1" == "s" ]; then
+	clang++ sequential.cpp
+elif [ "$1" == "p" ]; then
+	clang++ -Xpreprocessor -pthread pthread.cpp -lpthread
+elif [ "$1" == "o" ]; then
+	clang++ -Xpreprocessor -fopenmp openmp.cpp -lomp
+else
+	echo "Supply appropriate and correct arguments"
+	exit
+fi
