a7db0c45597ad9078fd56d1e1952fdebbb435c7e
Pradyumna Meena <pradyumnameena@gmail.com>
2020-02-21 23:24:44 +0530

    completed pthread part

--- A1/pthread.cpp
+++ A1/pthread.cpp
@@ -10,65 +10,70 @@ using namespace std;
 double limit = 1000;
 int thread_count = 1;
 
-struct threadArgs{
-int tid;
-int end;
-int start;
-int col;
-int idx;
-vector<double*> *array;
+struct threadArgs1{
+	int col;
+	int idx;
+	int end;
+	int start;
+	vector<double*> *array;
 };
 
 struct threadArgs2{
-int tid;
-int end;
-int start;
-int col;
-int idx;
-vector<double*> *l;
-vector<double*> *u;
-vector<double*> *mat;
+	int col;
+	int idx;
+	int end;
+	int start;
+	vector<double*> *lower;
+	vector<double*> *upper;
+	vector<double*> *mat;
 };
 
-void *func(void* threadarg){
-	struct threadArgs *args = (struct threadArgs*) threadarg;
-	int tid = args->tid;
-	int max, col, idx, i;
-	col = args->col,idx = args->idx;
-	for(i = start; i < end; i++){
-		// max = *(lower[col] + (i);
-		// *(lower[col] + i) = *(lower[idx] + i);
-		// *(lower[idx] + i) = max;
-		max = *(args->array[col] + i);
-		*(args->array[col] + i) = *(args->array[idx] + i);
-		*(args->array[idx] + i) = max;
+void *func1(void* threadarg){
+	struct threadArgs1 *args = (struct threadArgs1*)threadarg;
+	int col = args->col;
+	int idx = args->idx;
+	int end = args->end;
+	int start = args->start;
+	double temp;
+	
+	for(int i = start;i<end;i++){
+		temp = *((*args->array)[col] + i);
+		*((*args->array)[col] + i) = *((*args->array)[idx] + i);
+		*((*args->array)[idx] + i) = temp;
 	}
+	
+	pthread_exit(0);
 }
 
 void *func2(void* threadarg){
-	struct threadArgs* args = (struct threadArgs2*) threadarg;
-	int tid = args->tid;
-	int col, idx, i;
-	col = args->col,idx = args->idx;
-	for(i = start; i < end; i++){
-		// *(lower[i] + col) = (*(mat[i] + col))/(*(upper[col] + col));
-		// *(upper[col] + i) = *(mat[col] + i);
-		*(args->l[i] + col) = *(args->mat[i] + col) / (*(args->u[col] + col));
-		*(args->u[col] + i) = *(args->mat[col] + i);
+	struct threadArgs2 *args = (struct threadArgs2*)threadarg;
+	int col = args->col;
+	int idx = args->idx;
+	int end = args->end;
+	int start = args->start;
+	
+	for(int i = start;i<end;i++){
+		*((*args->lower)[i] + col) = *((*args->mat)[i] + col) / (*((*args->upper)[col] + col));
+		*((*args->upper)[col] + i) = *((*args->mat)[col] + i);
 	}
+	
+	pthread_exit(0);
 }
 
 void *func3(void* threadarg){
-	struct threadArgs* args = (struct threadArgs2*) threadarg;
-	int tid = args->tid;
-	int col, idx, i, j;
-	col = args->col,idx = args->idx;
-	for(i = start; i < end; i++){
-		for(j = start; j < end; j++){
-			// *(mat[i] + j) = *(mat[i] + j) - (*(lower[i] + col))*(*(upper[col] + j));
-			*(args->mat[i] + j) = *(args->mat[i] + j) - (*(args->l[i] + col)) * (*(args->upper[col] + j));
+	struct threadArgs2 *args = (struct threadArgs2*)threadarg;
+	int col = args->col;
+	int idx = args->idx;
+	int end = args->end;
+	int start = args->start;
+	
+	for(int i = start;i<end;i++){
+		for(int j = col+1;j<idx;j++){
+			*((*args->mat)[i] + j) = *((*args->mat)[i] + j) - (*((*args->lower)[i] + col))*(*((*args->upper)[col] + j));
 		}
 	}
+	
+	pthread_exit(0);
 }
 
 vector<double*> initialize(int n){
@@ -107,6 +112,31 @@ vector<double*> read_data(){
 	return matrix;
 }
 
+void write_data(vector<double*> matrix,string name){
+	int n = matrix.size();
+	ofstream file;
+	file.open("./" + name);
+	
+	if(file.is_open()){
+		file << n;
+		file << "\n";
+		for(int i = 0;i<n;i++){
+			for(int j = 0;j<n;j++){
+				file << *(matrix[i] + j);
+				if(j==n-1){
+					if(i!=n-1){
+						file << "\n";
+					}
+				}else{
+					file << " ";
+				}
+			}
+		}
+	}
+
+	file.close();
+}
+
 vector<double*> matrix_copy(vector<double*> m1){
 	int i,j,n;
 	n = m1.size();
@@ -143,11 +173,10 @@ double checker(vector<int>& perm, vector<double*> mat, vector<double*> lower, ve
 		temp = 0.0;
 		for(i = 0;i<n;i++){
 			v1 = *(mat[perm[i]] + j);
-			v2 = 0.0;
 			for(k = 0;k<n;k++){
-				v2+=((*(lower[i] + k)) * (*(upper[k] + j)));
+				v1-=((*(lower[i] + k)) * (*(upper[k] + j)));
 			}
-			temp+=((v2-v1)*(v2-v1));
+			temp+=(v1*v1);
 		}
 		ans+=sqrt(temp);
 	}
@@ -158,12 +187,24 @@ double checker(vector<int>& perm, vector<double*> mat, vector<double*> lower, ve
 void LUdecomp(vector<double*> mat, vector<int>& perm, vector<double*> lower, vector<double*> upper){
 	double max;
 	double* add;
-	int i,j,n,temp,idx,col;
+	int i,j,n,temp,idx,col,res;
+	int bounds,part;
 	
 	n = mat.size();
+	
+	pthread_t threads[thread_count];
+	struct threadArgs1 threadArr1[thread_count];
+	struct threadArgs2 threadArr2[thread_count];
 
+	for(int i = 0;i<thread_count;i++){
+		threadArr1[i].array = &lower;
+		threadArr2[i].lower = &lower;
+		threadArr2[i].upper = &upper;
+		threadArr2[i].mat = &mat;
+	}
+	
 	for(col = 0;col<n;col++){
-		cout << col << endl;
+		res = 0;
 		max = 0.0;
 		idx = col;
 		for(i = col;i<n;i++){
@@ -188,21 +229,61 @@ void LUdecomp(vector<double*> mat, vector<int>& perm, vector<double*> lower, vec
 
 		*(upper[col] + col) = *(mat[col] + col);
 
-		for(i = 0;i<col;i++){
-			max = *(lower[col] + i);
-			*(lower[col] + i) = *(lower[idx] + i);
-			*(lower[idx] + i) = max;
-		}
+		part = col/thread_count;
+		bounds = 0;
 
-		for(i = col+1;i<n;i++){
-			*(lower[i] + col) = (*(mat[i] + col))/(*(upper[col] + col));
-			*(upper[col] + i) = *(mat[col] + i);
-		}
-
-		for(i = col+1;i<n;i++){
-			for(j = col+1;j<n;j++){
-				*(mat[i] + j) = *(mat[i] + j) - (*(lower[i] + col))*(*(upper[col] + j));
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			threadArr1[thread_id].col = col;
+			threadArr1[thread_id].idx = idx;
+			threadArr1[thread_id].start = bounds;
+			threadArr1[thread_id].end = bounds + part;
+			if(thread_id==thread_count-1){
+				threadArr1[thread_id].end = col;
 			}
+			bounds+=part;
+			res = pthread_create(&threads[thread_id],NULL,func1,&threadArr1[thread_id]);
+		}
+
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			pthread_join(threads[thread_id],NULL);
+		}
+
+		part = (n-1-col)/thread_count;
+		bounds = col+1;
+
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			threadArr2[thread_id].col = col;
+			threadArr2[thread_id].idx = idx;
+			threadArr2[thread_id].start = bounds;
+			threadArr2[thread_id].end = bounds + part;
+			if(thread_id==thread_count-1){
+				threadArr2[thread_id].end = n;
+			}
+			bounds+=part;
+			res = pthread_create(&threads[thread_id],NULL,func2,&threadArr2[thread_id]);
+		}
+
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			pthread_join(threads[thread_id],NULL);
+		}
+		
+		part = (n-1-col)/thread_count;
+		bounds = col+1;
+
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			threadArr2[thread_id].col = col;
+			threadArr2[thread_id].idx = n;
+			threadArr2[thread_id].start = bounds;
+			threadArr2[thread_id].end = bounds + part;
+			if(thread_id==thread_count-1){
+				threadArr2[thread_id].end = n;
+			}
+			bounds+=part;
+			res = pthread_create(&threads[thread_id],NULL,func3,&threadArr2[thread_id]);
+		}
+
+		for(int thread_id = 0;thread_id<thread_count;thread_id++){
+			pthread_join(threads[thread_id],NULL);
 		}
 	}
 }
@@ -211,6 +292,7 @@ int main(int argc, char const *argv[]){
 	vector<int> perm;
 	vector<double*> lower;
 	vector<double*> upper;
+	vector<double*> perm_final;
 	struct timeval start, end;
 	double time_taken,time_taken2;
 	
@@ -231,14 +313,17 @@ int main(int argc, char const *argv[]){
 	for(i = 0;i<n;i++){
 		double* add1 = (double*)malloc(n*sizeof(double));
 		double* add2 = (double*)malloc(n*sizeof(double));
+		double* add3 = (double*)malloc(n*sizeof(double));
 		lower.push_back(add1);
 		upper.push_back(add2);
+		perm_final.push_back(add3);
 	}
 
 	for(i = 0;i<n;i++){
 		for(j = 0;j<n;j++){
 			*(upper[i] + j) = 0.0;
 			*(lower[i] + j) = 0.0;
+			*(perm_final[i] + j) = 0.0;
 		}
 		*(lower[i] + i) = 1.0;
 	}
@@ -249,7 +334,9 @@ int main(int argc, char const *argv[]){
 
 	time_taken = (end.tv_sec - start.tv_sec) * 1e6;
 	time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
-	cout << "Time taken by program is : " << time_taken << " sec" << endl; 
+	cout << "LU Decomposition in " << time_taken << " sec" << endl;
+
+	cout << check << endl;
 
 	if(check==1){
 		gettimeofday(&start, NULL);
@@ -262,4 +349,17 @@ int main(int argc, char const *argv[]){
 		cout << "L2,1 norm = " << ans << endl;
 		cout << "Time taken for checking : " << time_taken2 << " sec" << endl;
 	}
+
+	// Writing into files
+	for(int i = 0;i<n;i++){
+		*(perm_final[i] + perm[i]) = 1.0;
+	}
+
+	string P_name = "P_" + to_string(n) + "_" + to_string(thread_count) + ".txt";
+	string L_name = "L_" + to_string(n) + "_" + to_string(thread_count) + ".txt";
+	string U_name = "U_" + to_string(n) + "_" + to_string(thread_count) + ".txt";
+	
+	write_data(perm_final,P_name);
+	write_data(lower,L_name);
+	write_data(upper,U_name);
 }
